393ce5cfa68708b3e1d154c2e9af24da2cfd6420
Seth <seth.bbrendle@gmail.com>
2022-10-19 17:56:49 -0400

    Ruby OOP Inheritance

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,13 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+require "pry"
+
+class Car < Vehicle
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+
+end
+
+binding.pry
